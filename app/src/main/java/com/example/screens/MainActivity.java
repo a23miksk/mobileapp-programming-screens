ab6b5f24d141fed119c66a90bde8ecd3f33e5bf3
tjugosex <mikael.skaremyr.te1@hotmail.com>
2024-04-17 10:22:25 +0200

    Added EditText widget and some extras functionality

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button activityButton = findViewById(R.id.activityButton);
+        activityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Button clicked");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Daniel"); // Optional
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,15 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            int number = extras.getInt("number");
+            // Do something with the name and number
+            TextView textViewName = findViewById(R.id.name);
+            textViewName.setText(name);
+        }
 
     }
 }
